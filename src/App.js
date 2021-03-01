4931a7c1b7b5a3e998fbc0d2ba40875a99226d9f
RafiulFerdous <rafiulferdous434@gmail.com>
2021-03-01 12:04:33 +0600

    navber completted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import NavBar from './components/Navbar'
 
 function App() {
   return (
-    <h2>portfolio proj</h2>
+    <NavBar />
   )
 }
 
